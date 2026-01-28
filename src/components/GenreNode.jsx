1995ea98aa3c875897076e3e60de19272f759958
pavlosgeo <pav.georgiades@gmail.com>
2026-01-28 12:26:46 +0200

    #280126 Updated and created files/folders

---
+++ src/components/GenreNode.jsx
@@ -0,0 +1,3 @@
+export default function GenreNode({ data }) {
+  return <div style={{ padding: 10, background: '#222', color: '#fff', borderRadius: 10 }}>{data.label}</div>;
+}
---
+++ src/data/influences.js
@@ -0,0 +1,6 @@
+export const influences = [
+  { id: 'rock-punk', source: 'rock', target: 'punk' },
+  { id: 'punk-grunge', source: 'punk', target: 'grunge' },
+  { id: 'electronic-house', source: 'electronic', target: 'house' },
+  { id: 'electronic-techno', source: 'electronic', target: 'techno' },
+];
