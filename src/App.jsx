ac952cd40f9265df0c6c92611f3042e489f0491b
pavlosgeo <pav.georgiades@gmail.com>
2026-02-03 12:09:30 +0200

    #030226 Added react-router-dom for routing functionality and set up routes in App component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-
+import { Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
 import MapPage from './pages/MapPage';
 
 function App() {
-  return <MapPage />;
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/map" element={<MapPage />} />
+    </Routes>
+  );
 }
 
 export default App;
